083a299d9ee2d234a2986efea04cdf79830e5617
Bartek Gruba <grubabartek@gmail.com>
2022-05-11 14:22:19 +0200

    Praca domowa z 10.05.

---
+++ src/main/java/pl/sda/arp4/objects/MainObywatel.java
@@ -0,0 +1,32 @@
+package pl.sda.arp4.objects;
+
+import java.util.Scanner;
+
+// Stwórz klasę Obywatel z imieniem i numerem pesel (przyjmij String). Stworz pełny konstruktor oraz gettery, settery i metodę toString.
+//W klasie MainObywatel napisz nastepujaca logike:
+//
+//    Stworz scanner
+//    Popros uzytkownika o podanie imienia
+//    Popros uzytkownika o podanie numeru pesel
+//    Stworz obiekt klasy Obywatel uzywajac konstruktora
+//    Wypisz obiekt na ekran stosujac sout.
+
+
+public class MainObywatel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj imie:");
+        String podaneImie = scanner.next();
+
+        System.out.println("Podaj numer PESEL:");
+        String numerPesel = scanner.next();
+
+
+        Obywatel obywatel1 = new Obywatel("numerPesel");
+        obywatel1.imie = podaneImie;
+        obywatel1.setPesel(numerPesel);
+
+        System.out.println("Obywatel " + obywatel1.getImie() + " o numerze PESEL: " + obywatel1.getPesel());
+    }
+}
---
+++ src/main/java/pl/sda/arp4/objects/Obywatel.java
@@ -0,0 +1,36 @@
+package pl.sda.arp4.objects;
+
+public class Obywatel {
+
+    String imie;
+    private String pesel;
+
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public Obywatel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    @Override
+    public String toString() {
+        return "Obywatel{" +
+                "imie='" + imie + '\'' +
+                ", pesel='" + pesel + '\'' +
+                '}';
+    }
+}
